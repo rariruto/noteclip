d07f3156a991d6ac7bf8c65c4fa4f8bd6a4acea3
root <root@DESKTOP-JAA6JTA>
2024-12-19 15:51:03 +0900

    clip class add

--- django/code/iyama/noteclipper/admin.py
+++ django/code/iyama/noteclipper/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Class, User, Note
+from .models import Class, User, Note, Clip
 
 # Register your models here.
 admin.site.register(Class)
 admin.site.register(User)
 admin.site.register(Note)
+admin.site.register(Clip)
--- django/code/iyama/noteclipper/models.py
+++ django/code/iyama/noteclipper/models.py
@@ -31,3 +31,14 @@ class Note(models.Model):
         return self.title
     def get_absolute_url(self):
         return reverse('noteclipper:main')
+
+class Clip(models.Model):
+    file = models.CharField(max_length = 100)
+    note = models.CharField(max_length = 100)
+    class_type = models.CharField(max_length = 15)
+    pos = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('noteclipper:main')
