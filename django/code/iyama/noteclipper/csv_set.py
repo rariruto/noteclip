ac5760faffd3eeea0459f3e12467b1818636b28c
root <root@Rariruto-PC>
2025-01-02 17:55:08 +0900

    csv url setting complete

--- django/code/iyama/noteclipper/csv_set.py
+++ django/code/iyama/noteclipper/csv_set.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from models import User, Note,Class
+from .models import User, Note,Class
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.views import generic
@@ -9,6 +9,9 @@ import re
 import csv
 import pprint
 
-def load_csv():
-    with open("static/noteclipper/reference/cut_data/SKM_C360i23092810090_0102.csv") as f:
-        print(f.read())
+class csv_process:
+    def load_csv(request):
+        with open("static/noteclipper/reference/cut_data/SKM_C360i23092810090_0102.csv") as f:
+            print(f.read())
+
+        return redirect('noteclipper:home')
