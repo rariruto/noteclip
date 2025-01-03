4c4860ff339c7c93398376e915581d649d8cc4f8
root <root@Rariruto-PC>
2025-01-03 10:48:56 +0900

    csv adjast compoeted! second

--- django/code/iyama/noteclipper/csv_set.py
+++ django/code/iyama/noteclipper/csv_set.py
@@ -12,24 +12,32 @@ import pprint
 class csv_process:
     def load_csv(request):
         note_data = Note.objects.all()
+        csv_data = Clip.objects.all()
 
         for data in note_data:
             title = data.title[8:-4]
+            done=True
             print(title)
             with open("static/noteclipper/reference/cut_data/" + title + ".csv") as f:
+                done=True
+
                 c_data = (re.findall('.+,.*,.*,.*,.*,.*,.*' ,f.read()))
+                for check in csv_data:
+                    if title!=check.note:
+                        done=True
+                    else:
+                        done=False
+                        break
+
                 for check in c_data:
-                    if check.find(title)>=0:
+                    if done==True:
                         data_div = check.split(',')
-                        print(data_div)
-                        print(check)
+
                     
                         if float(data_div[4]) > 1500:
                             result = Clip(file= data_div[1] + "/" + data_div[0] + ".bmp", note=title, class_type=data_div[1], pos="0" )
                         else:
                             result = Clip(file= data_div[1] + "/" + data_div[0] + ".bmp", note=title, class_type=data_div[1], pos="1" )
                         result.save()
-
-                        print(c_data)
 
         return redirect('noteclipper:home')
